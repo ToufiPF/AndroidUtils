50e22a00369be05e93c2b7e25c685b6a70ef15ed
Aurélien Clergeot <aurelien.clergeot@epfl.ch>
2023-05-19 18:14:22 +0200

    Fix package name/path

--- AndroidTestUtils/src/androidTest/java/ch/epfl/toufi/android_test_utils/ExampleInstrumentedTest.kt
+++ AndroidTestUtils/src/androidTest/java/ch/epfl/toufi/android_test_utils/ExampleInstrumentedTest.kt
@@ -1,12 +1,10 @@
-package ch.epfl.android_test_utils
+package ch.epfl.toufi.android_test_utils
 
-import androidx.test.platform.app.InstrumentationRegistry
 import androidx.test.ext.junit.runners.AndroidJUnit4
-
+import androidx.test.platform.app.InstrumentationRegistry
+import org.junit.Assert.*
 import org.junit.Test
 import org.junit.runner.RunWith
-
-import org.junit.Assert.*
 
 /**
  * Instrumented test, which will execute on an Android device.
@@ -19,6 +17,6 @@ class ExampleInstrumentedTest {
     fun useAppContext() {
         // Context of the app under test.
         val appContext = InstrumentationRegistry.getInstrumentation().targetContext
-        assertEquals("ch.epfl.android_test_utils.test", appContext.packageName)
+        assertEquals("ch.epfl.toufi.android_test_utils.test", appContext.packageName)
     }
 }
